e35ec131c03e7644e46fb0dafabcb01aec7940b3
Gabriele <116716975+gabryx2z@users.noreply.github.com>
2022-11-04 10:57:31 +0100

    Add files via upload
    Esercitazione Gabriele Di giampietro

---
+++ Gioco.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (){
+
+
+int punteggio=0;
+int numero_domanda=0;
+int scelta;
+int risposta;
+int scelta_finale;
+char nickname;
+
+     printf ("Benvenuto in questo mini gioco dove dovrai rispondere a delle domande a risposta multipla\n");
+     printf ("inserisci 1 per una nuova partita\n inserisci 0 per uscire dal gioco\n");
+     scanf  ("%d", &scelta);
+    while  (scelta>1){
+     printf ("errore\n inserisci 1 per una nuova partita\n inserisci 0 per uscire dal gioco\n");
+     scanf  ("%d", &scelta);
+}
+
+    do { 
+     if(scelta==0){exit;}
+     if(scelta ==1){
+     printf ("inizio del gioco\n rispondi inserendo il numero della risposta corretta\n");
+     printf ("\nDomanda 1: quale è la capitale d'Italia?\n 1Torino\n 2Roma\n 3Genova\n");
+     scanf  ("%d", &risposta);
+     if (risposta !=2){
+     printf ("risposta errata\n");}
+     else {
+     printf ("risposta corretta\n");
+     punteggio++;}
+
+     printf("\nDomanda 2: quale è la capitale della Francia?\n 1Lione\n 2Marsiglia\n 3Parigi\n");
+     scanf ("%d", &risposta);
+     if (risposta!=3){
+     printf ("risposta errata\n");}
+     else{
+     printf ("risposta corretta\n");
+     punteggio++;}
+
+     printf("\nDomanda 3: quale è la capitale del regno unito?\n 1Glasgow\n 2Londra\n 3Edimburgo\n");
+     scanf ("%d", &risposta);
+     if (risposta!=2){
+     printf ("risposta errata\n");}
+     else{
+     printf ("risposta corretta\n");
+     punteggio++;}
+}
+
+     printf ("\nhai totalizzato %d punti\n",punteggio);
+     printf ("\nvuoi iniziare una nuova partita?\n inserisci 1 per una nuova partita\n se vuoi uscire inserisci 0\n");
+     scanf  ("%d", &scelta_finale);
+}
+    while (scelta_finale==1);
+}
